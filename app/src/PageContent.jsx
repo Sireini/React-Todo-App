9002e04f53917c4edccf9b830a580ce0903577d1
smgjreinieren <smgj.reinieren@gmail.com>
2016-11-23 23:18:35 +0100

    changes

--- app/src/PageContent.jsx
+++ app/src/PageContent.jsx
@@ -18,44 +18,13 @@ class PageContent extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-lg-8 col-lg-offset-2">
-
-                    
-
-                    <div className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-                        <div className="hovereffect">
-                            <img className="img-responsive" src="http://placehold.it/350x250" alt=""/>
-                                <div className="overlay">
-                                    <h2>Effect 12</h2>
-                                    <p> 
-                                        <a href="#">LINK HERE</a>
-                                    </p> 
-                                </div>
-                        </div>
-                    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                         <div className="list-view">
                             <p><strong>Display:</strong></p>
                             <a href="#" id="list"><span className="fa fa-th-list"></span></a>
                             <a href="#" id="grid"><span className="fa fa-th"></span></a>
                         </div>
 
-                        <h1 className="m-b-md">Fancy Toggle Sidebar Navigation</h1>
+                        <h1 className="m-b-md">Title has to be set</h1>
 
                             <div id="products" className="row list-group">
                                 <div className="item col-xs-4 col-lg-4">
